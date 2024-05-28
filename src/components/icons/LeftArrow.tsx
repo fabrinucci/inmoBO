4b920e47a37c4aee5d15a491d168daaa58b1c43d
Fabrizio <fabrinucci99@gmail.com>
2024-05-28 19:28:47 -0300

    feat: Add Icons

---
+++ src/components/icons/LeftArrow.tsx
@@ -0,0 +1,19 @@
+export const LeftArrow = () => {
+  return (
+    <svg
+      className='ms-2 h-3.5 w-3.5 rtl:rotate-180'
+      aria-hidden='true'
+      fill='none'
+      viewBox='0 0 14 10'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
+        d='M1 5h12m0 0L9 1m4 4L9 9'
+      />
+    </svg>
+  );
+};
